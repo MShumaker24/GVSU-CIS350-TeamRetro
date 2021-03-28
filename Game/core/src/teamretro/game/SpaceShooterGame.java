9d9eeae358d34c2e299060375f3e11322e7d3f50
Nicholas Terpstra <rterp2@gmail.com>
2021-03-28 13:40:21 -0400

    added rest of the space shooter game

--- Game/core/src/teamretro/game/SpaceShooterGame.java
+++ Game/core/src/teamretro/game/SpaceShooterGame.java
@@ -1,14 +1,13 @@
 package teamretro.game;
+import com.badlogic.gdx.Game;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import java.util.Random;
 
-public class Game extends com.badlogic.gdx.Game {
+public class SpaceShooterGame extends Game {
 
 	GameScreen gameScreen;
+
+	public static Random random = new Random();
 
 	@Override
 	public void create() {
--- Game/desktop/src/teamretro/game/desktop/DesktopLauncher.java
+++ Game/desktop/src/teamretro/game/desktop/DesktopLauncher.java
@@ -2,17 +2,17 @@ package teamretro.game.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import teamretro.game.Game;
+import teamretro.game.SpaceShooterGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 
-		/*
-		config.height = (insert height here);
-		config.width = (insert width here);
 
-		 */
-		new LwjglApplication(new Game(), config);
+		config.height = 640;
+		config.width = 360;
+
+
+		new LwjglApplication(new SpaceShooterGame(), config);
 	}
 }
