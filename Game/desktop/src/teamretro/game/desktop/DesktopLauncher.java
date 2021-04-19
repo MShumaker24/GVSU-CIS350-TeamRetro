b81837cb5ef9d5269f4f45cd4323d330d70b3eb8
mendelco <48532483+mendelco@users.noreply.github.com>
2021-04-19 19:45:17 -0400

    updated window size for new sprites

--- Game/desktop/src/teamretro/game/desktop/DesktopLauncher.java
+++ Game/desktop/src/teamretro/game/desktop/DesktopLauncher.java
@@ -8,8 +8,8 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 
-//		config.height = 640;
-//		config.width = 360;
+		config.height = 1080;
+		config.width = 640;
 
 		new LwjglApplication(new SpaceShooterGame(), config);
 	}
