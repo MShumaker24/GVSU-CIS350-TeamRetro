d7a5b63519b08dea9ec5e47d2ed2fb679278db90
EWeb07 <70358138+EWeb07@users.noreply.github.com>
2021-04-20 16:06:13 -0400

    Added Sound

--- Game/core/src/teamretro/game/PlayerShip.java
+++ Game/core/src/teamretro/game/PlayerShip.java
@@ -1,8 +1,10 @@
 package teamretro.game;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.Gdx;
 
 class PlayerShip extends Ship {
-
+    private Music Lsound;
     int lives;
 
     public PlayerShip(float xCentre, float yCentre,
@@ -19,6 +21,9 @@ class PlayerShip extends Ship {
 
     @Override
     public Laser[] fireLasers() {
+        Lsound=Gdx.audio.newMusic(Gdx.files.internal("laser8.wav"));
+        Lsound.setVolume(0.1f);
+        Lsound.play();
         Laser[] laser = new Laser[2];
         laser[0] = new Laser(boundingBox.x + boundingBox.width * 0.07f, boundingBox.y + boundingBox.height * 0.45f,
                 laserWidth, laserHeight,
@@ -32,3 +37,4 @@ class PlayerShip extends Ship {
         return laser;
     }
 }
+
